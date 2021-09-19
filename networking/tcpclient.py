8b5b57ab561cf208ee49955cac55e0a6ecd028a8
dasithsv <dasithsv@gmail.com>
2021-09-20 00:52:20 +0530

    completed tcp client

--- networking/tcpclient.py
+++ networking/tcpclient.py
@@ -7,11 +7,25 @@ port = 80
 
 
 client = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+
+'''
+Create socket object with AF_INET and SOCK_STREAM 
+
+- The AF_INET parameter indicates we’ll use a standard IPv4 address or host-name 
+- SOCK_STREAM indicates that this will be a TCP client 
+'''
+
 client.connect((host, port))
+# connecting to the host and port
 
 client.send((b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n"))
+# just sends a headers for the request
 
 response = client.recv(9001)
+# getting the response
 
-print(response)
+print(response.decode())
+# printing the resonse
+
 client.close()
+# close the connection
