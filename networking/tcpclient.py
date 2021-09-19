fc8dd40d993af2312c2e790ed2ee036540b6120d
dasithsv <dasithsv@gmail.com>
2021-09-20 03:06:07 +0530

    created a tcp-server

--- networking/tcpclient.py
+++ networking/tcpclient.py
@@ -1,29 +1,33 @@
 #!/usr/bin/python3
 import socket
 
-host = 'www.google.com'
-port = 80
+def main():
+    host = 'www.google.com'
+    port = 80
 
-client = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+    client = socket.socket(socket.AF_INET , socket.SOCK_STREAM)
+    
+    '''
+    Create socket object with AF_INET and SOCK_STREAM 
 
-'''
-Create socket object with AF_INET and SOCK_STREAM 
+    - The AF_INET parameter indicates we’ll use a standard IPv4 address or host-name 
+    - SOCK_STREAM indicates that this will be a TCP client 
+    '''
 
-- The AF_INET parameter indicates we’ll use a standard IPv4 address or host-name 
-- SOCK_STREAM indicates that this will be a TCP client 
-'''
+    client.connect((host, port))
+    # connecting to the host and port
 
-client.connect((host, port))
-# connecting to the host and port
+    client.send((b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n"))
+    # just sends a headers for the request
 
-client.send((b"GET / HTTP/1.1\r\nHost: www.google.com\r\n\r\n"))
-# just sends a headers for the request
+    response = client.recv(9001)
+    # getting the response
 
-response = client.recv(9001)
-# getting the response
+    print(response.decode())
+    # printing the resonse
 
-print(response.decode())
-# printing the resonse
+    client.close()
+    # close the connection
 
-client.close()
-# close the connection
+if __name__ == '__main__':
+    main()
--- networking/tcpserver.py
+++ networking/tcpserver.py
@@ -1,2 +1,41 @@
 #!/usr/bin/python3
 
+import socket
+import threading
+
+ip = '0.0.0.0'
+port = 9004
+
+def main():
+
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind ((ip, port))
+    server.listen(5)
+    print(f'[*] Listning on {ip}:{port}')
+
+    while True:
+        try:
+            # we get the client socket on client and connection details on address -> address can be something like ('127.0.0.1', 41410)
+            client, address = server.accept() 
+            print(f"[*] Got connection from {address[0]}:{address[1]}")
+
+            # create a new Thread object that points to handle_client funtion and we pass client ip as argument
+            client_handler = threading.Thread(target=handle_client, args=(client,)) 
+            client_handler.start()
+        except:
+            print('[-] Something went wrong')
+            exit()
+
+def handle_client(client_socket):
+    # do a recv and then send a simple message back
+    with client_socket as sock:
+        try:
+            request = sock.recv(1024)
+            print(f"[*] Received : {request.decode('utf-8')}")
+            sock.send(b'got it! ')
+        except:
+            print("[-] Something went wrong")
+            exit()
+
+if __name__ == '__main__':
+    main()
